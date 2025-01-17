bbaac46d02ea8f7b2fbd7e8238e80677d412e6d9
Spynder <Spynder@users.noreply.github.com>
2025-01-17 15:12:37 +0300

    differentiated decimal and binary

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,89 +1,92 @@
 $(document).ready(function() {
-    $("#game").hide();
-    $("#start").click(function() {
-        $("#firstPage").hide();
-        $("#game").show();
-        collectSettings();
-        startGame();
-    });
+	$("#game").hide();
+	$("#start").click(function() {
+		$("#firstPage").hide();
+		$("#game").show();
+		collectSettings();
+		startGame();
+	});
 
-    let settings = {};
-    function collectSettings() {
-        settings["binaryDigits"] = parseInt($("#optionBinaryDigits").val());
-        settings["maxCorrectAnswers"] = parseInt($("#optionMaxCorrectAnswers").val());
-        settings["penalty"] = parseInt($("#optionPenalty").val());
-        settings["backwards"] = $("#optionBackwards").is(":checked");
-    }
+	let settings = {};
+	function collectSettings() {
+		settings["binaryDigits"] = parseInt($("#optionBinaryDigits").val());
+		settings["maxCorrectAnswers"] = parseInt($("#optionMaxCorrectAnswers").val());
+		settings["penalty"] = parseInt($("#optionPenalty").val());
+		settings["backwards"] = $("#optionBackwards").is(":checked");
+	}
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * max);
+	}
 
-    let start;
-    let timer;
-    let running = false;
-    function startGame() {
-        running = true;
-        showNumber();
-        start = Date.now();
-        timer = setInterval(function() {
-            let passed = Date.now() - start + (total-correct)*settings["penalty"]*1000;
-            let milliseconds = (passed%1000).toString().padStart(3, '0');
-            let seconds = (Math.floor(passed/1000) % 60).toString().padStart(2, '0');
-            let minutes = (Math.floor(passed/60000)).toString().padStart(2, '0');
-            $("#timer").text(`${minutes}:${seconds}.${milliseconds}`);
-        }, 1./60.);
-        $("#input").focus();
-    }
+	let start;
+	let timer;
+	let running = false;
+	function startGame() {
+		running = true;
+		showNumber();
+		start = Date.now();
+		timer = setInterval(function() {
+			let passed = Date.now() - start + (total-correct)*settings["penalty"]*1000;
+			let milliseconds = (passed%1000).toString().padStart(3, '0');
+			let seconds = (Math.floor(passed/1000) % 60).toString().padStart(2, '0');
+			let minutes = (Math.floor(passed/60000)).toString().padStart(2, '0');
+			$("#timer").text(`${minutes}:${seconds}.${milliseconds}`);
+		}, 1./60.);
+		$("#input").focus();
+	}
 
-    function stopGame() {
-        clearInterval(timer);
-        running = false;
-    }
+	function stopGame() {
+		clearInterval(timer);
+		running = false;
+	}
 
-    let currNumber = -1;
-    let total = 0;
-    let correct = 0;
-    function showNumber() {
-        currNumber = getRandomInt(Math.pow(2, settings["binaryDigits"]));
-        if(settings["backwards"] && getRandomInt(2) == 1) {
-            $("#number").text(currNumber);
-            currNumber = currNumber.toString(2).padStart(settings["binaryDigits"], '0');
-        } else
-            $("#number").text(currNumber.toString(2).padStart(settings["binaryDigits"], '0'));
-    }
+	let currNumber = -1;
+	let total = 0;
+	let correct = 0;
+	function showNumber() {
+		currNumber = getRandomInt(Math.pow(2, settings["binaryDigits"]));
+		if(settings["backwards"] && getRandomInt(2) == 1) {
+			$("#number").text(currNumber);
+			$("#number").addClass("decimal");
+			currNumber = currNumber.toString(2).padStart(settings["binaryDigits"], '0');
+		} else {
+			$("#number").text(currNumber.toString(2).padStart(settings["binaryDigits"], '0'));
+			$("#number").removeClass("decimal");
+		}
+	}
 
-    function pressedNumber(num) {
-        total+=1;
-        if(num == currNumber || parseInt(num) == currNumber) {
-            $("#hint").text("Correct!");
-            $("#hint").addClass("correct");
-            $("#hint").removeClass("wrong");
-            correct++;
-        } else {
-            $("#hint").text("Wrong!");
-            $("#hint").addClass("wrong");
-            $("#hint").removeClass("correct");
-        }
-        $("#counter").text(`${correct}/${total}`);
-        if(correct >= settings["maxCorrectAnswers"]) {
-            stopGame();
-            return;
-        }
-        showNumber();
-    }
+	function pressedNumber(num) {
+		total+=1;
+		if(num == currNumber || parseInt(num) == currNumber) {
+			$("#hint").text("Correct!");
+			$("#hint").addClass("correct");
+			$("#hint").removeClass("wrong");
+			correct++;
+		} else {
+			$("#hint").text("Wrong!");
+			$("#hint").addClass("wrong");
+			$("#hint").removeClass("correct");
+		}
+		$("#counter").text(`${correct}/${total}`);
+		if(correct >= settings["maxCorrectAnswers"]) {
+			stopGame();
+			return;
+		}
+		showNumber();
+	}
 
-    function isNumeric(value) {
-        return /^-?\d+$/.test(value);
-    }
+	function isNumeric(value) {
+		return /^-?\d+$/.test(value);
+	}
 
-    $("#input").on("change keyup paste", function(e) {
-        if(!running) return;
-        let text = e.target.value;
-        //alert("?")
-        if(text.indexOf("\n") != -1 || text.indexOf(" ") != -1 && isNumeric(text) && text != "") {
-            pressedNumber(text);
-            $("#input").val("");
-        }
-    });
+	$("#input").on("change keyup paste", function(e) {
+		if(!running) return;
+		let text = e.target.value;
+		//alert("?")
+		if(text.indexOf("\n") != -1 || text.indexOf(" ") != -1 && isNumeric(text) && text != "") {
+			pressedNumber(text);
+			$("#input").val("");
+		}
+	});
 });
